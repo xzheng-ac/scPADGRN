833a0339619e403cdee742db07101392dbaf3fa0
xzheng-ac <59996642+xzheng-ac@users.noreply.github.com>
2020-05-13 12:29:12 +0800

    Update script_dataset2.R

--- data/dataset2/script_dataset2.R
+++ data/dataset2/script_dataset2.R
@@ -28,6 +28,6 @@ for(i in c(1:(N-1))){
     net[[i]][abs(net[[i]])<0.2268]<-0
     net[[i]] <- abs(net[[i]])
     net[[i]][net[[i]]>0] <- 1
-    net[[i]][net[[i]]<0] <- 1
+    net[[i]][net[[i]]<0] <- -1
 }
 
